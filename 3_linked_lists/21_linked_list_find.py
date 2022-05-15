b4f72554952b86fc0203135e08e4459c156279c5
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-15 22:39:03 +0530

    L21: Add tests

--- 3_linked_lists/21_linked_list_find.py
+++ 3_linked_lists/21_linked_list_find.py
@@ -98,3 +98,46 @@ def linked_list_find_recursive(head, target):
     if head.val == target:
         return True
     return linked_list_find_recursive(head.next, target)
+
+
+def test_00():
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+
+    a.next = b
+    b.next = c
+    c.next = d
+
+    # a -> b -> c -> d
+    assert linked_list_find(a, "c") == True
+    assert linked_list_find(a, "q") == False
+
+
+def test_01():
+    node1 = Node("jason")
+    node2 = Node("leneli")
+
+    node1.next = node2
+
+    # jason -> leneli
+    linked_list_find(node1, "jason") == True
+
+
+def test_02():
+    node1 = Node(42)
+
+    # 42
+    assert linked_list_find(node1, 42) == True
+    assert linked_list_find(node1, 100) == False
+
+
+def run_tests():
+    test_00()
+    test_01()
+    test_02()
+    print("All tests passed")
+
+
+run_tests()
