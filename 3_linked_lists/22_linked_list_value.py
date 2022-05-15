d8b6a293d73cba78b5689803d197db0d702bce0e
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-15 22:48:19 +0530

    L22: Add tests

--- 3_linked_lists/22_linked_list_value.py
+++ 3_linked_lists/22_linked_list_value.py
@@ -86,3 +86,67 @@ def get_node_value_iterative(head, index):
         curr_index += 1
 
     return None if curr is None else curr.val
+
+
+# test_00
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+# a -> b -> c -> d
+
+assert get_node_value(a, 2) == 'c'
+
+# test_01:
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+# a -> b -> c -> d
+
+assert get_node_value(a, 3) == 'd'
+
+# test_02:
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+# a -> b -> c -> d
+
+assert get_node_value(a, 7) == None
+
+# test_03:
+node1 = Node("banana")
+node2 = Node("mango")
+
+node1.next = node2
+
+# banana -> mango
+
+assert get_node_value(node1, 0) == 'banana'
+
+# test_04:
+node1 = Node("banana")
+node2 = Node("mango")
+
+node1.next = node2
+
+# banana -> mango
+
+assert get_node_value(node1, 1) == 'mango'
+print("All tests passed")
