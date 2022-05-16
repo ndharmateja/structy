d43facc0664c4aa352bab4d89458a0acc0a68141
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-16 17:37:56 +0530

    L23: Format file

--- 3_linked_lists/23_reverse_list.py
+++ 3_linked_lists/23_reverse_list.py
@@ -39,9 +39,9 @@ reverse_list(p) # p
 
 
 class Node:
-  def __init__(self, val):
-    self.val = val
-    self.next = None
+    def __init__(self, val):
+        self.val = val
+        self.next = None
 
 
 def reverse_list(head):
