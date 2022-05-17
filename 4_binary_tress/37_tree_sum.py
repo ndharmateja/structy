99f7850f4ddd8b958b23ddd0e59a82514efff276
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-17 22:49:59 +0530

    L37: Add tests

--- 4_binary_tress/37_tree_sum.py
+++ 4_binary_tress/37_tree_sum.py
@@ -66,3 +66,57 @@ def tree_sum(root):
     if root is None:
         return 0
     return root.val + tree_sum(root.left) + tree_sum(root.right)
+
+
+# test_00:
+a = Node(3)
+b = Node(11)
+c = Node(4)
+d = Node(4)
+e = Node(-2)
+f = Node(1)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#       3
+#    /    \
+#   11     4
+#  / \      \
+# 4   -2     1
+
+assert tree_sum(a) == 21
+# test_01:
+a = Node(1)
+b = Node(6)
+c = Node(0)
+d = Node(3)
+e = Node(-6)
+f = Node(2)
+g = Node(2)
+h = Node(2)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+e.left = g
+f.right = h
+
+#      1
+#    /   \
+#   6     0
+#  / \     \
+# 3   -6    2
+#    /       \
+#   2         2
+
+assert tree_sum(a) == 10
+# test_02:
+assert tree_sum(None) == 0
+
+print("All tests passed")
