5cdc10c43ea64bd9876f2d7c8c2ecb3adda6018e
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-19 11:02:25 +0530

    L37: Add tests

--- 4_binary_tress/37_depth_first_values.py
+++ 4_binary_tress/37_depth_first_values.py
@@ -103,6 +103,7 @@ def depth_first_values(root):
     # Recursive 2
     return depth_first_values_recursive2(root)
 
+
 def depth_first_values_iterative(root):
     if root is None:
         return []
@@ -135,3 +136,79 @@ def depth_first_values_recursive2(root):
         return []
 
     return [root.val] + depth_first_values_recursive2(root.left) + depth_first_values_recursive2(root.right)
+
+
+# test_00:
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+assert depth_first_values(a) == ['a', 'b', 'd', 'e', 'c', 'f']
+# test_01:
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+g = Node('g')
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+e.left = g
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+#    /
+#   g
+
+assert depth_first_values(a) == ['a', 'b', 'd', 'e', 'g', 'c', 'f']
+# test_02:
+a = Node('a')
+#     a
+assert depth_first_values(a) == ['a']
+# test_03:
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+a.right = b
+b.left = c
+c.right = d
+d.right = e
+
+#      a
+#       \
+#        b
+#       /
+#      c
+#       \
+#        d
+#         \
+#          e
+
+assert depth_first_values(a) == ['a', 'b', 'c', 'd', 'e']
+# test_04:
+assert depth_first_values(None) == []
+
+print("All tests passed")
