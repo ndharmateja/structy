ea13f584e7c91e7b0a173faa38e8262978ade30a
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-19 10:49:59 +0530

    L36: Add tests

--- 4_binary_tress/36_breadth_first_values.py
+++ 4_binary_tress/36_breadth_first_values.py
@@ -116,3 +116,88 @@ def breadth_first_values(root):
             queue.put(curr.right)
 
     return values
+
+
+# test_00:
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+assert breadth_first_values(a) == ['a', 'b', 'c', 'd', 'e', 'f']
+# test_01:
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+g = Node('g')
+h = Node('h')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+e.left = g
+f.right = h
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+#    /       \
+#   g         h
+
+assert breadth_first_values(a) == ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+# test_02:
+a = Node('a')
+
+#      a
+
+assert breadth_first_values(a) == ['a']
+# test_03:
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+x = Node('x')
+
+a.right = b
+b.left = c
+c.left = x
+c.right = d
+d.right = e
+
+#      a
+#       \
+#        b
+#       /
+#      c
+#    /  \
+#   x    d
+#         \
+#          e
+
+assert breadth_first_values(a) == ['a', 'b', 'c', 'x', 'd', 'e']
+# test_04:
+assert breadth_first_values(None) == []
+
+print("All tests passed")
