e14db6877c13b4f46f4527673ba5709b6bbfe118
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-19 10:47:44 +0530

    L36: Solve

---
+++ 4_binary_tress/36_breadth_first_values.py
@@ -0,0 +1,118 @@
+'''
+breadth first values
+Write a function, breadth_first_values, that takes in the root of a binary tree. The function should return a list containing all values of the tree in breadth-first order.
+
+test_00:
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+breadth_first_values(a) 
+#    -> ['a', 'b', 'c', 'd', 'e', 'f']
+test_01:
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+g = Node('g')
+h = Node('h')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+e.left = g
+f.right = h
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+#    /       \
+#   g         h
+
+breadth_first_values(a) 
+#   -> ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+test_02:
+a = Node('a')
+
+#      a
+
+breadth_first_values(a) 
+#    -> ['a']
+test_03:
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+x = Node('x')
+
+a.right = b
+b.left = c
+c.left = x
+c.right = d
+d.right = e
+
+#      a
+#       \
+#        b
+#       /
+#      c
+#    /  \
+#   x    d
+#         \
+#          e
+
+breadth_first_values(a) 
+#    -> ['a', 'b', 'c', 'x', 'd', 'e']
+test_04:
+breadth_first_values(None) 
+#    -> []
+'''
+from queue import Queue
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def breadth_first_values(root):
+    if root is None:
+        return []
+
+    values = []
+    queue = Queue()
+    queue.put(root)
+    while not queue.empty():
+        curr = queue.get()
+        values.append(curr.val)
+
+        if curr.left is not None:
+            queue.put(curr.left)
+        if curr.right is not None:
+            queue.put(curr.right)
+
+    return values
